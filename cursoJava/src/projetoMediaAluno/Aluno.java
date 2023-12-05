552b6f3a327635c7e632cc6d0a099a93fd060b25
lucas <lucas@DESKTOP-JNB5S8E>
2023-12-04 23:32:38 -0300

    implementando set e get na classe Aluno

---
+++ cursoJava/src/projetoMediaAluno/Aluno.java
@@ -0,0 +1,97 @@
+package projetoMediaAluno;
+
+public class Aluno {
+	
+private String nome;
+private int idade;
+private String dataNascimento;
+private String registroGeral;
+private String numeroCpf;
+private String nomePai;
+private String nomeMae;
+private String nomeEscola;
+private String serieMatriculado;
+
+public Aluno() {
+	// TODO Auto-generated constructor stub
+}
+
+public Aluno(String nomeP) {
+	nome = nomeP;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public int getIdade() {
+	return idade;
+}
+
+public void setIdade(int idade) {
+	this.idade = idade;
+}
+
+public String getDataNascimento() {
+	return dataNascimento;
+}
+
+public void setDataNascimento(String dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+
+public String getRegistroGeral() {
+	return registroGeral;
+}
+
+public void setRegistroGeral(String registroGeral) {
+	this.registroGeral = registroGeral;
+}
+
+public String getNumeroCpf() {
+	return numeroCpf;
+}
+
+public void setNumeroCpf(String numeroCpf) {
+	this.numeroCpf = numeroCpf;
+}
+
+public String getNomePai() {
+	return nomePai;
+}
+
+public void setNomePai(String nomePai) {
+	this.nomePai = nomePai;
+}
+
+public String getNomeMae() {
+	return nomeMae;
+}
+
+public void setNomeMae(String nomeMae) {
+	this.nomeMae = nomeMae;
+}
+
+public String getNomeEscola() {
+	return nomeEscola;
+}
+
+public void setNomeEscola(String nomeEscola) {
+	this.nomeEscola = nomeEscola;
+}
+
+public String getSerieMatriculado() {
+	return serieMatriculado;
+}
+
+public void setSerieMatriculado(String serieMatriculado) {
+	this.serieMatriculado = serieMatriculado;
+}
+
+
+
+}
---
+++ cursoJava/src/projetoMediaAluno/MediaTest.java
@@ -0,0 +1,15 @@
+package projetoMediaAluno;
+
+public class MediaTest {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("mauricio"); 
+		
+		System.out.println("meu nome e : " + aluno1.getNome());
+		
+		
+	}
+
+}
